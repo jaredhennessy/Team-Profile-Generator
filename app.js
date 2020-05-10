62341a57e5d76632c084b268d20190208394df8d
jaredhennessy <jared.hennessy@gmail.com>
2020-05-09 17:51:55 -0700

    classes fully defined

--- app.js
+++ app.js
@@ -14,6 +14,62 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+inquirer.prompt([
+  {
+    type: "list",
+    name: "role",
+    message: "What is the Employee's role?",
+    choices: ["Manager", "Engineer", "Intern"],
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "What is the Employee's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the Employee's ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the Employee's email address?",
+  },
+  {
+    type: "input",
+    // type: "number",
+    name: "officeNumber",
+    message: "What is the Manager's office phone number?",
+    // validate: function (value) {
+    //   var valid = !isNaN(parseFloat(value));
+    //   return (
+    //     valid || "Please enter a phone number without dashes or parentheses."
+    //   );
+    // },
+    // filter: Number,
+    when: function (answers) {
+      return answers.role === "Manager";
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the Engineer's GitHub username?",
+    when: function (answers) {
+      return answers.role === "Engineer";
+    },
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What school does the Intern attend?",
+    when: function (answers) {
+      return answers.role === "Intern";
+    },
+  },
+]);
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
