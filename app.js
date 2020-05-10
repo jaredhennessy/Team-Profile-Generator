28e29b5e29df7ba956bd4e01d2ea292a490bbe76
jaredhennessy <jared.hennessy@gmail.com>
2020-05-09 17:07:53 -0700

    lib development

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//jshint esversion:6
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -9,7 +10,6 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
